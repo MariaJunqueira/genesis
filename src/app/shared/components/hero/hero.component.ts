3bfb67a2a7cbb738870d6c9c0d90b51cf6499bdc
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-10-06 13:10:45 +0200

    creating hero component

---
+++ src/app/shared/components/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [TranslateModule, ButtonComponent],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+
+}

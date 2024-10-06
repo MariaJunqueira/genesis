cfd5f89f575403e5b0d3e05d1d6652cfba924819
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-10-06 14:28:56 +0200

    creating tile component

---
+++ src/app/shared/components/tile/tile.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { Tile } from './tile.model';
+
+@Component({
+  selector: 'app-tile',
+  standalone: true,
+  templateUrl: './tile.component.html',
+  styleUrl: './tile.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TranslateModule]
+})
+export class TileComponent {
+  tile = input.required<Tile>();
+}
---
+++ src/app/shared/components/tile/tile.model.ts
@@ -0,0 +1,5 @@
+export interface Tile {
+  title: string;
+  description: string;
+  icon: string;
+}

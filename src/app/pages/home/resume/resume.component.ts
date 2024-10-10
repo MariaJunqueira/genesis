832fa4dadcba7d2c0fa5783ff32f87a2b9679cfd
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-10-10 16:21:33 +0200

    update tile to make it more generical

--- src/app/pages/home/resume/resume.component.ts
+++ src/app/pages/home/resume/resume.component.ts
@@ -2,14 +2,41 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { TileGridComponent } from '../../../shared/components/tile-grid/tile-grid.component';
+import { Tile } from '../../../shared/components/tile/tile.model';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [ButtonComponent, TranslateModule],
+  imports: [ButtonComponent, TileGridComponent, TranslateModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
-
+  tiles: Array<Tile> = [
+    {
+      heading: 'home::services::webDesign::heading',
+      title: 'home::services::webDesign::title',
+      subtitle: 'home::services::webDesign::subtitle',
+      description: 'home::services::webDesign::description',
+    },
+    {
+      heading: 'home::services::uiuxDesign::heading',
+      title: 'home::services::uiuxDesign::title',
+      subtitle: 'home::services::uiuxDesign::subtitle',
+      description: 'home::services::uiuxDesign::description',
+    },
+    {
+      heading: 'home::services::productDesign::heading',
+      title: 'home::services::productDesign::title',
+      subtitle: 'home::services::productDesign::subtitle',
+      description: 'home::services::productDesign::description',
+    },
+    {
+      heading: 'home::services::webDevelopment::heading',
+      title: 'home::services::webDevelopment::title',
+      subtitle: 'home::services::webDevelopment::subtitle',
+      description: 'home::services::webDevelopment::description',
+    }
+  ];
 }
--- src/app/shared/components/tile/tile.model.ts
+++ src/app/shared/components/tile/tile.model.ts
@@ -1,5 +1,7 @@
 export interface Tile {
+  heading?: string;
   title: string;
+  subtitle?: string;
   description: string;
-  icon: string;
+  icon?: string;
 }

b386acb111f17eaaafa210ab0b95645a5774cf88
MariaJunqueira <mariaa.junqueira@gmail.com>
2025-11-29 13:39:07 +0100

    feat: add new 'About' page with dedicated components, content, images, translations, and routing (#5)

--- src/app/pages/home/about-me/about-me.component.ts
+++ src/app/pages/home/about-me/about-me.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
     selector: 'app-about-me',
-    imports: [ButtonComponent, TranslateModule],
+    imports: [ButtonComponent, RouterLink, TranslateModule],
     templateUrl: './about-me.component.html',
     styleUrl: './about-me.component.scss'
 })
